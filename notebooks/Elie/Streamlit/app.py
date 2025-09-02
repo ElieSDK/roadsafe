3bd29f0fb3cdffbda1602cc40e5865566d6d9a69
Elie Sadaka <e.sdk.mail@gmail.com>
2025-09-02 15:54:33 +0900

    fixed use_container_width=True

--- notebooks/Elie/Streamlit/app.py
+++ notebooks/Elie/Streamlit/app.py
@@ -155,7 +155,7 @@ if uploaded_file:
     uploaded_file.seek(0)
     file_bytes = uploaded_file.read()
     image = Image.open(io.BytesIO(file_bytes)).convert("RGB")
-    st.image(image, caption="Uploaded Image", width=st.columns(1)[0].width)
+    st.image(image, caption="Uploaded Image", width=None)
 
     # Prediction
     with st.spinner("Predicting..."):
